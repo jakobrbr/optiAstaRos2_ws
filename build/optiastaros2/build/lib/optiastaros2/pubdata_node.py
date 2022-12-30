d9b6388ecd1d66e6cad42cbea7dad94fea2e1c61
Jakob Rud Bernhardt <jakobrb@outlook.dk>
2022-12-30 19:07:46 +0100

    yes

--- build/optiastaros2/build/lib/optiastaros2/pubdata_node.py
+++ build/optiastaros2/build/lib/optiastaros2/pubdata_node.py
@@ -1,5 +1,45 @@
-def main():
-    print('Hi from optiastaros2.')
+#!/usr/bin/env python3
+import rclpy
+import timeit
+from rclpy.node import Node
+from rigidbody_msgs import rigidbody
+#from geometry_msgs.msg import Pose2D
+from NatNetClient import NatNetClient
+id_dict = dict() # dictionary for positions of rigid bodies
+
+def receiveRigidBodyFrame(id, position, rotation):
+    id_dict[str(id)] = [position,rotation] # update the position and orientation of rigid body
+
+class PublishDataNode(Node):
+
+    def __init__(self):
+        super().__init__("pubdata_node")
+        self.cmd_vel_pub_ = self.create_publisher(rigidbody, "/data", 10)
+        self.timer_ = self.create_timer(0.005, self.send_data)
+        self.get_logger().info("NatNet data publisher node has been started")
+
+    def send_data(self):
+        streamingClient = NatNetClient(ver=(3, 0, 0, 0), quiet=True)
+        streamingClient.rigidBodyListener = receiveRigidBodyFrame
+        streamingClient.run()
+        msg = rigidbody()
+        for i in id_dict.keys():
+            pos,rot = id_dict[i]
+            msg.pose.x = pos[0]
+            msg.pose.y = pos[1]
+            msg.pose.z = pos[2]
+            msg.rot.x = rot[0]
+            msg.rot.y = rot[1]
+            msg.rot.z = rot[2]
+            msg.rigid_body_name = i
+        self.cmd_vel_pub_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PublishDataNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
