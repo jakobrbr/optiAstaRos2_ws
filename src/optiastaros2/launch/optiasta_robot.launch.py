fc0aa4e7df60b93d5f4a048ef199d0470336d4d0
Jakob Rud Bernhardt <43971775+jakobrbr@users.noreply.github.com>
2023-01-05 15:45:07 +0100

    Update optiasta_robot.launch.py

--- src/optiastaros2/launch/optiasta_robot.launch.py
+++ src/optiastaros2/launch/optiasta_robot.launch.py
@@ -8,22 +8,22 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='optiastaros2', node_executable='pubdata_node', output='screen'),
+            package='optiastaros2', executable='pubdata_node', output='screen'),
 
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot1', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot1', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot2', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot2', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot3', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot3', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot4', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot4', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot5', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot5', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot6', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot6', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot7', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot7', output='screen'),
         Node(
-            package='optiastaros2', node_executable='controller_node', namespace='robot8', output='screen'),
+            package='optiastaros2', executable='controller_node', namespace='robot8', output='screen'),
     ])
