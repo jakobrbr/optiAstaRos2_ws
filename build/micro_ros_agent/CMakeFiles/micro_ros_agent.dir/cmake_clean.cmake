ed44f0756efddffa8ad68192ac4454108a6924e8
Jakob Rud Bernhardt <jakobrb@outlook.dk>
2023-01-06 16:36:00 +0100

    Removed micro_ros_package

---
+++ build/micro_ros_agent/CMakeFiles/micro_ros_agent.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/micro_ros_agent"
+  "CMakeFiles/micro_ros_agent-complete"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-build"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-configure"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-download"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-install"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-mkdir"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-patch"
+  "micro_ros_agent-prefix/src/micro_ros_agent-stamp/micro_ros_agent-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/micro_ros_agent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
