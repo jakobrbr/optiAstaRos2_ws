87181288ffff0444d774c8f155b6139858aae901
Jakob Rud Bernhardt <43971775+jakobrbr@users.noreply.github.com>
2023-01-13 12:44:37 +0100

    Update latency_tester.py

--- src/optiastaros2/optiastaros2/latency_tester.py
+++ src/optiastaros2/optiastaros2/latency_tester.py
@@ -1,28 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+#from std_msgs.msg import String
+from std_msgs.msg import Int32
 # This node is used to test the latency when sending a message wirelessly 
-# from Raspberry Pi to ESP32 using ROS2-microROS
+# from Raspberry Pi to ESP32 and back, using ROS2-microROS
 
 class LatencyTester(Node):
 
     def __init__(self):
         super().__init__('latency_tester')
-        self.publisher_ = self.create_publisher(String, 'latency_topic', 10)
+        self.publisher_ = self.create_publisher(Int32, 'ping_topic', 10)
+        self.subscriber = self.create_subscription(Int32, "latency_topic", self.latency_callback, 1)
         timer_period = 0.1  # s
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.sent_time = None
 
     def timer_callback(self):
-        msg = String()
-        msg.data = "ping"
+        msg = Int32()
+        msg.data = 1234
         self.sent_time = self.get_clock().now().nanoseconds
         self.publisher_.publish(msg)
 
-    def latency_callback(self, msg):
+    def latency_callback(self, msgnew: Int32):
         receive_time = self.get_clock().now().nanoseconds
         latency = receive_time - self.sent_time
-        self.get_logger().info("Latency: {} ns".format(latency))
+        self.get_logger().info("Recieved message: " + msgnew.data + " Latency: {} ns".format(latency))
 
 def main(args=None):
     rclpy.init(args=args)
