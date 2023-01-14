0c24f5d095e1751c0220d8f06abcadd9830b3a17
Jakob Rud Bernhardt <43971775+jakobrbr@users.noreply.github.com>
2023-01-14 19:16:38 +0100

    Update controller_node.py

--- src/optiastaros2/optiastaros2/controller_node.py
+++ src/optiastaros2/optiastaros2/controller_node.py
@@ -135,7 +135,7 @@ class ControllerNode(Node):
                 #self.velocity[j] = velocity_controller(self.currentPos[j],self.targetPosArr[j],self.start_time,self.lap_time, 1) # for constant velocity set: velocyty = 1 
 
 
-                self.velocity[j] = 0.5 # constant low linear velocity, maybe set to 1 again or 0.5
+                self.velocity[j] = 0.75 # constant low linear velocity, maybe set to 1 again or 0.5
 
 
                 #self.velocity[j] *= pure_pursuit_turn_speed(self.last_angle[j],self.angle[j]) # turn controller
