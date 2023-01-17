107e76f8271d8c39263e7de1fab09b0b5ca012f3
Jakob Rud Bernhardt <43971775+jakobrbr@users.noreply.github.com>
2023-01-17 17:01:46 +0100

    Update controller_node.py

--- src/optiastaros2/optiastaros2/controller_node.py
+++ src/optiastaros2/optiastaros2/controller_node.py
@@ -109,7 +109,7 @@ class ControllerNode(Node):
     
     def robot0_callback(self, msg: RigidBody):
         n = 0 # this is the callback for robot n
-        lookahead_distance = 6 # lookahead, in number of indeces
+        lookahead_distance = 12 # lookahead, in number of indeces
         velocity = 1.0 # constant linear velocity, should be float!
 
         if self.targetPosArr[n]: # maybe not needed to check
